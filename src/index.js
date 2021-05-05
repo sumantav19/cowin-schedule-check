987f81059dc28e9f1b631eb3245d5ca9a5958406
Sumant Dey <sdey@equalexperts.com>
2021-05-05 16:48:14 +0530

    Added gitignore

--- src/index.js
+++ src/index.js
@@ -2,6 +2,35 @@ const https = require("https");
 const cron = require("node-cron");
 const player = require("play-sound")();
 const basename = 'https://cdn-api.co-vin.in';
+
+function postRequest(data){
+    return new Promise((resolve)=>{
+        let responseData='';
+        
+        const options = {
+            hostname: basename,
+            path: '/todos',
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Content-Length': data.length
+            }
+    
+        }
+        const req = https.request(options,res=>{
+            res.on('data', d => {
+                 responseData+=d;
+              })
+            res.on('end',()=>{
+    
+            })
+        })
+        req.write(data);
+        req.end();
+    })
+    
+
+}
 function getSchedule(date,districtId){
     let data = '';
     https.get(`${basename}/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${date}`,res=>{
@@ -10,12 +39,12 @@ function getSchedule(date,districtId){
         })
 
         res.on('end',()=>{
-            console.log("parsing...");
+            process.stdout.write("parsing...");
             data  = JSON.parse(data);
             let sessions = data.sessions;
             sessions.map((slot)=>{
                 if(slot.min_age_limit === 18 && slot.available_capacity > 0){
-                    console.log(`${slot.pincode}   ${slot.vaccine} ${slot.name}  ${slot.available_capacity}`)
+                    console.log(`\n ${slot.pincode}   ${slot.vaccine} ${slot.name} available ${slot.available_capacity}`)
                     player.play('./media/338.mp3', err=>{
                         if(err)console.log(`Could not play: ${err}`);
                     })
@@ -27,14 +56,11 @@ function getSchedule(date,districtId){
 
 function cronRun(){
     console.log('starting');
-    const task = cron.schedule('* * * * *',() =>{
-        getSchedule('03-05-2021','294');
-        console.log("+++++++++");
+    const task = cron.schedule('*/5 * * * * *',() =>{
+        getSchedule('05-05-2021','294');
     });
     task.start();
 }
 
 cronRun();
-// getSchedule('03-05-2021','294');
-
 // getSchedule;
