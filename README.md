6a6f50273ec8249abe5f4172009942975ed22fd3
Sumant Dey <sdey@equalexperts.com>
2021-05-14 14:07:14 +0530

    Update the api to calendar by district

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 ### Steps to run.
 - Install Node
-- Run ```npm install```
+- Open Command prompt or Terminal, Git clone the repo
+- Run inside the folder```npm install```
 - Refer this blog to install [Audio player](https://thisdavej.com/node-js-playing-sounds-to-provide-notifications/)
     * Keep in mind for windows <b> MPlayer </b> is required  
-- Update districtId and date at ```getSchedule('date','districtId')``` in src/index.js
-- Run ```node .```
+- Run inside the folder ```node . <date> <districtId> <Token>```
 
 ### Notes
 - Schedule runs depends on cron syntax
 - Test the audio player
 - Get the District id from cowin websit
 - Date format ```dd-mm-yyyy```
+- Token is not mandatory
+- There is a change in api call. There could be a lag of 30 mins to get the latest data
--- src/index.js
+++ src/index.js
@@ -1,82 +1,65 @@
+const { O_TRUNC } = require("constants");
 const https = require("https");
 const cron = require("node-cron");
 const player = require("play-sound")();
 const basename = 'https://cdn-api.co-vin.in';
-/// 
-// function postRequest(data){
-//     return new Promise((resolve)=>{
-//         let responseData='';
-        
-//         const options = {
-//             hostname: basename,
-//             path: '/todos',
-//             method: 'POST',
-//             headers: {
-//               'Content-Type': 'application/json',
-//               'Content-Length': data.length
-//             }
-    
-//         }
-//         const req = https.request(options,res=>{
-//             res.on('data', d => {
-//                  responseData+=d;
-//               })
-//             res.on('end',()=>{
-    
-//             })
-//         })
-//         req.write(data);
-//         req.end();
-//     })
-    
 
-// }
-function getSchedule(date, districtId) {
-    const options = {
+function getSchedule(date, districtId, token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        // 'Content-Length': data.length,
+        'Referrer': 'https://www.cowin.gov.in/',
+        'Origin': 'https://www.cowin.gov.in',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+        ...(token && { 'authorization': `Bearer ${token}` }),
+        // 'If-None-Match':'W/"27d3a-g+fud61pnexS4+LVM2DXyEa1ai4"',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0'
+    };
+
+    let options = {
         method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Length': data.length,
-            // 'Referrer': 'https://www.cowin.gov.in/',
-            // 'Origin': 'https://www.cowin.gov.in',
-            // 'If-None-Match':'W/"27d3a-g+fud61pnexS4+LVM2DXyEa1ai4"',
-            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0'
-        }
+        headers: headers
     }
     let data = '';
-    const req = https.request(`${basename}/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${date}`, options, res => {
+    const req = https.request(`${basename}/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${districtId}&date=${date}`, options, res => {
         res.on('data', d => {
             data += d;
         })
-        // console.log("Response headers",res.headers)
-        // console.log("Response status",res.status)
+        // console.log("Response headers", res.headers)
+        // console.log("Response status", res.statusCode)
         res.on('end', () => {
             process.stdout.write("parsing...");
-            data = JSON.parse(data);
-            let sessions = data.sessions;
-            sessions.map((slot) => {
-                if (slot.min_age_limit === 18 && slot.available_capacity > 0) {
-                    console.log(`\n ${slot.pincode}   ${slot.vaccine} ${slot.name} available ${slot.available_capacity}`)
-                    player.play('./media/338.mp3', err => {
-                        if (err) console.log(`Could not play: ${err}`);
+            if (res.statusCode === 200) {
+                data = JSON.parse(data);
+                let centers = data.centers;
+                centers.map((center) => {
+                    const sessions = center.sessions;
+                    sessions.map(session => {
+                        if (session.min_age_limit > 18 && session.available_capacity > 0) {
+                            console.log(`\n ${center.pincode}   ${session.vaccine} ${center.name} available ${session.available_capacity} ${session.date}`)
+                            player.play('./media/338.mp3', err => {
+                                if (err) console.log(`Could not play: ${err}`);
+                            })
+                        }
                     })
-                }
-            })
+
+                })
+            } else { console.log(`response status is ${res.statusCode}, please update the token and rerun`) }
         })
-        req.on('error',(e)=>{
+        req.on('error', (e) => {
             console.log("Error in request>>>>>", e);
         })
     })
     req.end();
 }
 
-function cronRun() {
-    console.log('starting');
-    const task = cron.schedule('*/5 * * * * *', () => {
-        getSchedule('date', 'DistrictId');
+function cronRun(date, districtId, token) {
+    console.log(`starting for ${date} ${districtId} ${token}`);
+
+    const task = cron.schedule('*/20 * * * * *', () => {
+        getSchedule(date, districtId, token);
     });
     task.start();
 }
-
-cronRun();
-// getSchedule('date', 'DistrictId');
+cronRun(process.argv[2],process.argv[3],process.argv[4]);
+// getSchedule(process.argv[2], process.argv[3], process.argv[4]);
