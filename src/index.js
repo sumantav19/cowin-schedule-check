5533c475c490f732c427e3390dc658ea4d0e70ab
Sumant Dey <sdey@equalexperts.com>
2021-05-16 12:10:10 +0530

    Add cache check

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-const { O_TRUNC } = require("constants");
 const https = require("https");
 const cron = require("node-cron");
 const player = require("play-sound")();
 const basename = 'https://cdn-api.co-vin.in';
 
+let task;
+let etag;
 function getSchedule(date, districtId, token) {
     let headers = {
         'Content-Type': 'application/json',
         // 'Content-Length': data.length,
-        'Referrer': 'https://www.cowin.gov.in/',
-        'Origin': 'https://www.cowin.gov.in',
+        'referrer': 'https://www.cowin.gov.in/',
+        'origin': 'https://www.cowin.gov.in',
         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
         ...(token && { 'authorization': `Bearer ${token}` }),
-        // 'If-None-Match':'W/"27d3a-g+fud61pnexS4+LVM2DXyEa1ai4"',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0'
+        ...(etag && {'if-none-match':etag}),
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0'
     };
 
     let options = {
@@ -23,14 +24,15 @@ function getSchedule(date, districtId, token) {
 	//${basename}/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${districtId}&date=${date}
     let data = '';
     const req = https.request(`${basename}/api/v2/appointment/sessions/calendarByDistrict?district_id=${districtId}&date=${date}`, options, res => {
+        etag = res.headers['etag']
         res.on('data', d => {
             data += d;
         })
         // console.log("Response headers", res.headers)
         // console.log("Response status", res.statusCode)
         res.on('end', () => {
-            process.stdout.write("parsing...");
             if (res.statusCode === 200) {
+                process.stdout.write("parsing...");
                 data = JSON.parse(data);
 				//console.log(JSON.stringify(data))
 				// && session.vaccine != "COVAXIN"
@@ -41,14 +43,19 @@ function getSchedule(date, districtId, token) {
                     sessions.map(session => {
                         if (session.min_age_limit == 18 && session.available_capacity > 0 && session.available_capacity_dose1 > 0) {
                             console.log(`\n ${center.pincode}   ${session.vaccine} ${center.name}   AGE:${session.min_age_limit}   available ${session.available_capacity} ${session.date}  ${center.fee_type}  ${date.getHours()}:${date.getMinutes()}.${date.getSeconds()}`)
-                            player.play('./media/loud-alarm-tone.mp3', err => {
+                            player.play('./media/338.mp3', err => {
                                 if (err) console.log(`Could not play: ${err}`);
                             })
                         }
                     })
 
                 })
-            } else { console.log(`response status is ${res.statusCode}, please update the token and rerun`)
+            } else if (res.statusCode == 304){
+                // do nothing
+                process.stdout.write(` status code ${res.statusCode}`)
+            }
+            else { console.log(`response status is ${res.statusCode}, please update the token and rerun`)
+                    task.end();
 					player.play('./media/Smoke Alarm.mp3', err => {
                                 if (err) console.log(`Could not play: ${err}`);
                             })
@@ -64,7 +71,7 @@ function getSchedule(date, districtId, token) {
 function cronRun(date, districtId, token) {
     console.log(`starting for ${date} ${districtId} ${token}`);
 
-    const task = cron.schedule('*/10 * * * * *', () => {
+    task = cron.schedule('*/20 * * * * *', () => {
         getSchedule(date, districtId, token);
     });
     task.start();
